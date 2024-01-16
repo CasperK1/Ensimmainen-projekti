f5f170ead4fdcefffb2cacc7d91b30e2253b1d7a
CasperK1 <casper141@gmail.com>
2024-01-16 17:51:31 +0200

    Tehtävät

--- Tehtävät - Moduuli 02/5. Massa.py
+++ Tehtävät - Moduuli 02/5. Massa.py
@@ -6,3 +6,6 @@ naulat_si= naulat * 425.6
 luodit_si= luodit * 13.3
 leiviskat_si= leiviskat * 8512
 
+grammat=naulat_si+luodit_si+leiviskat_si
+print('Massa nykymittojen mukaan:')
+print((int(grammat//1000)),'kilogrammaa ja', round(grammat%1000, 2),'grammaa.')
---
+++ Tehtävät - Moduuli 02/6. Arvonta.py
@@ -0,0 +1,8 @@
+import random
+
+for x in range(3):
+    print(random.randint(1, 9), end='')
+print()
+for x in range(4):
+    print(random.randint(1, 6), end='')
+
