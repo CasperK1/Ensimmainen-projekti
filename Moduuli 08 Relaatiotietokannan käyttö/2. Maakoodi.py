ff40d719ebb0c817e13e582aefdc8707e7a04876
CasperK1 <156774036+CasperK1@users.noreply.github.com>
2024-02-01 18:21:25 +0200

    moduuli08: 1-3

--- Moduuli 08 Relaatiotietokannan käyttö/2. Maakoodi.py
+++ Moduuli 08 Relaatiotietokannan käyttö/2. Maakoodi.py
@@ -18,38 +18,67 @@ def kentat(maakoodi):
     tulos = []
     yk, kursori = yhteys()
 
-    sql = f"SELECT type, COUNT(*) FROM airport WHERE airport.iso_country = '{maakoodi}' GROUP BY type"
+    sql = (f"SELECT type, COUNT(*) FROM airport "
+           f"WHERE airport.iso_country = '{maakoodi}' "
+           f"GROUP BY type")
     kursori.execute(sql)
     tulos = kursori.fetchall()
     return tulos
 
 
+kenttatyypit = {
+    'closed': 'Suljettuja kenttiä',
+    'balloonport': 'Kuumailmapallokenttiä',
+    'heliport': 'Helikopterikenttiä',
+    'large_airport': 'Isoja lentokenttiä',
+    'medium_airport': 'Keskikokoisia kenttiä',
+    'seaplane_base': 'Vesitasokenttiä',
+    'small_airport': 'Pieniä kenttiä'
+}
+
 maakoodi = input('Syötä 2-kirjaiminen maakoodi: ').upper()
 tulos = kentat(maakoodi)
 
-i = 0
 print(f'Maassa {maakoodi} on:\n')
-for _ in tulos:
-    if tulos[i][0] == 'closed':
-        print(f'Suljettuja kenttiä {tulos[i][1]} kpl')
-        i += 1
-    elif tulos[i][0] == 'balloonport':
-        print(f'Kuumailmapallokenttiä {tulos[i][1]} kpl')
-        i += 1
-    elif tulos[i][0] == 'heliport':
-        print(f'Helikopterikenttiä {tulos[i][1]} kpl')
-        i += 1
-    elif tulos[i][0] == 'large_airport':
-        print(f'Isoja lentokenttiä {tulos[i][1]} kpl')
-        i += 1
-    elif tulos[i][0] == 'medium_airport':
-        print(f'Keskikokoisia kenttiä {tulos[i][1]} kpl')
-        i += 1
-    elif tulos[i][0] == 'seaplane_base':
-        print(f'Vesitasokenttiä {tulos[i][1]} kpl')
-        i += 1
-    elif tulos[i][0] == 'small_airport':
-        print(f'Pieniä kenttiä {tulos[i][1]} kpl')
-        i += 1
-    else:
-        i += 1
+for kentta, tulos in tulos:
+    if kentta in kenttatyypit:
+        print(f'{kenttatyypit[kentta]}: {tulos} kpl')
+
+
+
+
+
+
+
+
+
+
+
+
+
+# i = 0
+# print(f'Maassa {maakoodi} on:\n')
+# for _ in tulos:
+#     if tulos[i][0] == 'closed':
+#         print(f'Suljettuja kenttiä {tulos[i][1]} kpl')
+#         i += 1
+#     elif tulos[i][0] == 'balloonport':
+#         print(f'Kuumailmapallokenttiä {tulos[i][1]} kpl')
+#         i += 1
+#     elif tulos[i][0] == 'heliport':
+#         print(f'Helikopterikenttiä {tulos[i][1]} kpl')
+#         i += 1
+#     elif tulos[i][0] == 'large_airport':
+#         print(f'Isoja lentokenttiä {tulos[i][1]} kpl')
+#         i += 1
+#     elif tulos[i][0] == 'medium_airport':
+#         print(f'Keskikokoisia kenttiä {tulos[i][1]} kpl')
+#         i += 1
+#     elif tulos[i][0] == 'seaplane_base':
+#         print(f'Vesitasokenttiä {tulos[i][1]} kpl')
+#         i += 1
+#     elif tulos[i][0] == 'small_airport':
+#         print(f'Pieniä kenttiä {tulos[i][1]} kpl')
+#         i += 1
+#     else:
+#         i += 1
