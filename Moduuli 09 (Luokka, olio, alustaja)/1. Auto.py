66ebd143f8202ed6b87d2f0e7920dbf333386d82
CasperK1 <156774036+CasperK1@users.noreply.github.com>
2024-02-03 19:07:17 +0200

    moduuli09: 1-4

--- Moduuli 09 (Luokka, olio, alustaja)/1. Auto.py
+++ Moduuli 09 (Luokka, olio, alustaja)/1. Auto.py
@@ -1,10 +1,13 @@
 class Auto:
-    def __init__(self, reknro, hnopeus=0, matka=0):
+    def __init__(self, reknro, huippunopeus):
         self.reknro = reknro
-        self.hnopeus = hnopeus
-        self.matka = matka
+        self.huippunopeus = huippunopeus
+        self.hetkellinen_nopeus = 0
+        self.matka = 0
 
 
 auto_1 = Auto('ABC-123', 142)
 print(f'Auton rekisterinumero: {auto_1.reknro}')
-print(f'Huippunopeus {auto_1.hnopeus} km/h')
+print(f'Huippunopeus: {auto_1.huippunopeus} km/h')
+print(f'Auton nopeus: {auto_1.hetkellinen_nopeus} km/h')
+print(f'Kuljettu matka: {auto_1.matka} km')
---
+++ Moduuli 09 (Luokka, olio, alustaja)/2. Auton kiihdytys.py
@@ -0,0 +1,31 @@
+class Auto:
+    def __init__(self, reknro, huippunopeus, hetkellinen_nopeus=0, matka=0):
+        self.reknro = reknro
+        self.huippunopeus = huippunopeus
+        self.hetkellinen_nopeus = hetkellinen_nopeus
+        self.matka = matka
+
+    def nopeus(self, kiihdytä):
+        self.hetkellinen_nopeus += kiihdytä
+        if self.hetkellinen_nopeus > self.huippunopeus:
+            self.hetkellinen_nopeus = self.huippunopeus
+            return
+        if self.hetkellinen_nopeus < 0:
+            self.hetkellinen_nopeus = 0
+            return
+
+
+auto_1 = Auto('ABC-123', 142)
+auto_1 = Auto('ABC-123', 142)
+print(f'Auton rekisterinumero: {auto_1.reknro}')
+print(f'Huippunopeus: {auto_1.huippunopeus} km/h')
+print(f'Auton nopeus: {auto_1.hetkellinen_nopeus} km/h')
+print(f'Kuljettu matka: {auto_1.matka} km\n')
+
+print('Kiihdytys...')
+auto_1.nopeus(30)
+auto_1.nopeus(70)
+auto_1.nopeus(50)
+print(f'Auton nopeus {auto_1.hetkellinen_nopeus} km/h\n')
+auto_1.nopeus(-200)
+print(f'Hätäjarrutus! Auton nopeus {auto_1.hetkellinen_nopeus} km/h')
